9d73761cc286c5a89eede4e396fdbcdbd8d2a85b
havetomoveon <safdarian23@gmail.com>
2023-06-13 17:42:02 +0330

    2048

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 class Data():
     def __init__(self):
         self.validList = []
@@ -13,6 +14,16 @@ class Data():
             else:
                 self.index = list(np.random.randint(0,9,3))
         self.index.sort()
+    def addNewButtonPositions(self):
+        er = [0,2,6,8]
+        inValidNums = list(self.geosButt.keys())
+        i = random.choice(er)
+        if not i in inValidNums:
+            if i % 4 == 0:
+                self.geosButt.update({i:[self.validList[i],2]})
+                self.geometrysTaken.append(self.validList[i])
+                return 
+
     def buttGeos(self):
         for i in self.index:
             self.geosButt.update({i:[self.validList[i],2]})
@@ -29,80 +40,52 @@ class Data():
             self.validList.append((x,y,buttWidth,buttHeight))
             x +=  (buttWidth + 5)       
             x_num += 1 
-    def validDirectories(self):
-        default = [] 
-        #[down,up,right,left]
-        down = 0
-        up = 0
-        right = 0
-        left = 0
-        for i in self.index:
-            if  abs(i + 3) < len(self.validList):
-                if self.validList[i + 3] in self.geometrysTaken:
-                    default.append(False)
-                else:default.append(True)
-            else:default.append(True)
-            down = 0
-            if abs(i - 3) == i - 3:
-                if self.validList[i - 3] in self.geometrysTaken:
-                    default.append(False)
-                else:default.append(True)
-            else:default.append(True)
-            up = 0
-            if abs(i + 1) < len(self.validList):
-                if self.validList[i + 1] in self.geometrysTaken:
-                    default.append(False)
-                else:default.append(True)
-            else:default.append(True)
-            right = 0
-            if abs(i - 1) == i - 1:
-                if self.validList[i - 1] in self.geometrysTaken:
-                    default.append(False)
-                else:default.append(True)
-            else:default.append(True)
-            left = 0
-            self.validDiretions.append(default)
-            default = []
     def getGeosButt(self):
         return self.geosButt
+    def changeButtonPosition(self, i, t):
+        self.geosButt.update({i + t : [self.validList[i + t],self.geosButt.get(i)[1]]})
+        self.geosButt.pop(i)
+        self.geometrysTaken.remove(self.validList[i])
+        self.geometrysTaken.append(self.validList[i + t])
+    def changeButtonPositionEqual(self, i, t):
+        firstButton = self.geosButt.get(i)[1]
+        secondButton = self.geosButt.get(i + t)[1]
+        if firstButton == secondButton :
+            self.geosButt.pop(i)
+            self.geosButt.update({i + t : [self.validList[i + t],firstButton + secondButton]})
+            self.geometrysTaken.remove(self.validList[i])
     def down(self):
         old  = list(self.geosButt.keys())
-        for i in old:
+        for i in reversed(old):
             if  abs(i + 3) < len(self.validList):
                 if not self.validList[i + 3] in self.geometrysTaken:
-                    self.geosButt.pop(i)
-                    self.geosButt.update({i + 3 : [self.validList[i + 3],2]})
-                    self.geometrysTaken.remove(self.validList[i])
-                    self.geometrysTaken.append(self.validList[i + 3])
+                    self.changeButtonPosition(i, 3)
+                else:
+                    self.changeButtonPositionEqual(i, 3)
     def up(self):
         old  = list(self.geosButt.keys())
-        for i in old:
+        for i in reversed(old):
             if abs(i - 3) == i - 3:
                 if not self.validList[i - 3] in self.geometrysTaken:
-                    self.geosButt.pop(i)
-                    self.geosButt.update({i - 3 : [self.validList[i - 3],2]})
-                    self.geometrysTaken.remove(self.validList[i])
-                    self.geometrysTaken.append(self.validList[i - 3])
+                   self.changeButtonPosition(i, -3)
+                else:
+                    self.changeButtonPositionEqual(i, -3)
     def right(self):
         old  = list(self.geosButt.keys())
-        for i in old:
+        for i in reversed(old):
             if not (i + 1) % 3 == 0:
                 if abs(i + 1) < len(self.validList):
                     if not self.validList[i + 1] in self.geometrysTaken:
-                        self.geosButt.pop(i)
-                        self.geosButt.update({i + 1 : [self.validList[i + 1],2]})
-                        self.geometrysTaken.remove(self.validList[i])
-                        self.geometrysTaken.append(self.validList[i + 1])
-                    
-
+                        self.changeButtonPosition(i, 1)
+                    else:
+                        self.changeButtonPositionEqual(i, 1)
     def left(self):
         old  = list(self.geosButt.keys())
-        for i in old:
+        for i in reversed(old):
             if not (i) % 3 == 0:
                 if abs(i - 1) == i - 1:
                     if not self.validList[i - 1] in self.geometrysTaken:
-                        self.geosButt.pop(i)
-                        self.geosButt.update({i - 1 : [self.validList[i - 1],2]})
-                        self.geometrysTaken.remove(self.validList[i])
-                        self.geometrysTaken.append(self.validList[i - 1])              
+                        self.changeButtonPosition(i, -1)
+                    else:
+                        self.changeButtonPositionEqual(i, -1)              
         
--- main.py
+++ main.py
@@ -5,7 +5,85 @@ import sys
 from time import sleep
 import numpy as np
 import data
-class mouseTracker(QObject):
+
+class moveButtons(QObject):
+    sig = Signal(QEvent)
+    def __init__(self,data):
+        super().__init__()
+        self.status = False
+        self.wichbutt = 0
+        self.validnumright = 0
+        self.validnumleft = 0
+        self.validnumup = 0
+        self.validnumdown = 0
+        self.mouseEventPositions = []
+        self.data = data
+    def addButtonUi(self):
+        #self.data.data.right()
+        self.data.data.addNewButtonPositions()
+        self.data.setButtons()
+    def eventFilter(self,o,e):
+
+        if self.status and e.type() == e.Type.MouseMove:
+            mouse = QMouseEvent(e).position()
+            #print(mouse)
+            if abs(mouse.x()) != mouse.x() and abs(mouse.y()) != mouse.y():
+                return       
+            self.mouseEventPositions.append(mouse.toTuple())
+            if len(self.mouseEventPositions) > 50:
+                w = self.mouseEventPositions[-1][0] - self.mouseEventPositions[0][0] 
+                r = self.mouseEventPositions[-1][1] - self.mouseEventPositions[0][1]
+                
+                g = [self.validnumright,
+                    self.validnumleft,
+                    self.validnumup,
+                    self.validnumdown]
+                if sum(g) == 10:
+                    g.sort()
+                    if g[-1] == self.validnumright:
+                        self.data.data.right()
+                        self.addButtonUi()
+                    if g[-1] == self.validnumleft:
+                        self.data.data.left()
+
+                        self.addButtonUi()
+                    if g[-1] == self.validnumup:
+                        self.data.data.up()                        
+                        self.addButtonUi()
+                    if g[-1] == self.validnumdown:
+                        self.data.data.down()
+                        self.addButtonUi()
+                    self.validnumright = 0
+                    self.validnumleft = 0
+                    self.validnumup = 0
+                    self.validnumdown = 0
+
+                if abs(w) > abs(r):
+                    if abs(w) != w:
+                        self.validnumleft += 1
+                        return True
+                    else:
+                        self.validnumright += 1
+                        return True
+                if abs(w) < abs(r):
+                    if abs(r) != r:
+                        self.validnumup += 1
+                        return True
+                    else:
+                        self.validnumdown += 1
+                        return True    
+        if e.type() == e.Type.MouseButtonPress and o.objectName() == "butt":
+            if o.isWindowType():
+                return True
+            if o.windowType() == QFrame:
+                return True
+            self.sig.emit(e)
+            self.status = True
+    @Slot()
+    def call(self):
+        self.mouseEventPositions = []
+        self.status = False
+class mouseNoButtonSignal(QObject):
     sig = Signal()
     def __init__(self,rt:QApplication):
         super().__init__()
@@ -18,93 +96,7 @@ class mouseTracker(QObject):
             if self.rt.mouseButtons() == QApplication.mouseButtons().NoButton:
                 self.sig.emit()
                 break
-            sleep(0.1)            
-
-class moveButtons(QObject):
-    sig = Signal(QEvent)
-
-    def __init__(self,data):
-        super().__init__()
-        self.status = False
-        self.wichbutt = 0
-        self.validnumright = 0
-        self.validnumleft = 0
-        self.validnumup = 0
-        self.validnumdown = 0
-        self.mouseEventPositions = []
-        self.data = data
-    def eventFilter(self,o:QPushButton,e:QEvent):
-        if self.status and e.type() == e.Type.MouseMove:
-            mouse = QMouseEvent(e).position()
-            #print(mouse)
-            if abs(mouse.x()) != mouse.x():
-                return
-            self.mouseEventPositions.append(mouse.toTuple())
-            if len(self.mouseEventPositions) > 60:
-
-                w = self.mouseEventPositions[-1][0] - self.mouseEventPositions[-59][0] 
-                r = self.mouseEventPositions[-1][1] - self.mouseEventPositions[-59][1]
-                
-                g = [self.validnumright,
-                    self.validnumleft,
-                    self.validnumup,
-                    self.validnumdown]
-                if sum(g) == 16:
-                    g.sort()
-                    print(g)
-                    if g[-1] == self.validnumright:
-                        self.data.data.right()
-                        self.data.setButtons()
-                        print("right")
-                    if g[-1] == self.validnumleft:
-                        self.data.data.left()
-                        self.data.setButtons()
-                        print("left")
-                    if g[-1] == self.validnumup:
-                        self.data.data.up()
-                        self.data.setButtons()
-                        print("up")
-                    if g[-1] == self.validnumdown:
-                        print("down")
-                        self.data.data.down()
-                        self.data.setButtons()
-
-                    self.validnumright = 0
-                    self.validnumleft = 0
-                    self.validnumup = 0
-                    self.validnumdown = 0
-
-                if abs(w) > abs(r):
-                    if abs(w) != w:
-                        self.validnumleft += 1
-                        return
-                    else:
-                        self.validnumright += 1
-                        return
-                if abs(w) < abs(r):
-                    if abs(r) != r:
-                        self.validnumup += 1
-                        return
-                    else:
-                        self.validnumdown += 1
-                        return
-        
-            
-                
-        if e.type() == e.Type.MouseButtonPress:
-            if o.isWindowType():
-                return
-            #if o.windowType() == QFrame:
-                #return
-            print(o)
-            self.sig.emit(e)
-            self.status = True
-
-    @Slot()
-    def call(self):
-        self.mouseEventPositions = []
-        self.status = False
-
+            sleep(0.01)            
 class frame(QFrame):
     def __init__(self,parent):
         super().__init__(parent)
@@ -119,10 +111,10 @@ class ui():
         self.mainframe = frame(main)
         self.mainframe.resize(main.size())
         self.tableframe = QFrame(self.mainframe)
-        sizeWidth = self.main.size().width()/2
-        sizeHeight = self.main.size().height()/2
-        x = sizeWidth * 0.50
-        y = sizeHeight * 0.50
+        sizeWidth = self.main.size().width()/1.5
+        sizeHeight = self.main.size().height()/1.5
+        x = sizeWidth * 0.250
+        y = sizeHeight * 0.250
         self.buttList = []
         self.data.validGeometry(sizeWidth,sizeHeight)  
         self.data.buttGeos()
@@ -134,13 +126,13 @@ class ui():
             j.setStyleSheet("color: gray;font-size: 35px;border:none;")
             self.nodeButtons.append(j)
         self.setButtons()
-
     def setButtons(self):
         for i in self.nodeButtons:
             i.hide()
         for d in self.data.getGeosButt():
             b = self.data.getGeosButt().get(d)
             but = self.nodeButtons[d]
+            but.setObjectName("butt")
             but.setText(str(b[1]))
             but.setGeometry(QRect(b[0][0],b[0][1],b[0][2],b[0][3]))
             but.setStyleSheet("color: gray;font-size: 35px")
@@ -153,13 +145,12 @@ class ui():
 
 
 if __name__ == "__main__":
-    app = QApplication()
-    
+    app = QApplication() 
     main = QMainWindow()
     i = ui(main)
     event = moveButtons(i)
     thread = QThread()
-    motra = mouseTracker(app)
+    motra = mouseNoButtonSignal(app)
     motra.moveToThread(thread)
     #thread.started.connect(motra)
     event.sig.connect(motra.run)
